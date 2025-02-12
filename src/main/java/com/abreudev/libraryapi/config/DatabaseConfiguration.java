d875e723e5751b56a266febc978666cc16b20f86
Felipe Abreu <abreufelipe12@hotmail.com>
2025-02-12 12:36:24 -0300

    Database configuration

---
+++ src/main/java/com/abreudev/libraryapi/config/DatabaseConfiguration.java
@@ -0,0 +1,41 @@
+package com.abreudev.libraryapi.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfiguration {
+
+    @Value("${spring.datasource.url}")
+    String url;
+    @Value("${spring.datasource.username}")
+    String username;
+    @Value("${spring.datasource.password}")
+    String password;
+    @Value("${spring.datasource.driver-class-name}")
+    String driver;
+
+    @Bean
+    public DataSource hikariDataSource(){
+
+        HikariConfig config = new HikariConfig();
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(driver);
+        config.setJdbcUrl(url);
+
+        config.setMaximumPoolSize(10); // maximo de conexões liberadas
+        config.setMinimumIdle(1); // tamanho inicial do pool
+        config.setPoolName("library-db-pool");
+        config.setMaxLifetime(600000); // 600 mil ms (10 minutos)
+        config.setConnectionTimeout(100000); // timeout para conseguir uma conexão
+        config.setConnectionTestQuery("select 1"); // query de teste
+
+        return new HikariDataSource(config);
+    }
+}
